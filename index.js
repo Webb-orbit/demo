82e3315722fdc3f9d0d8cf15b17e8b05cf72feb3
webb-orbit <webbenemies@gmail.com>
2024-08-09 11:36:16 +0530

    add img and table

--- index.js
+++ index.js
@@ -12,9 +12,22 @@ const plaintohtml =(markdown)=>{
             .replace(/```/g, '&grave;')
             .replace(/'/g, '&#039;');
     }
+
+    const parseTable = (input) => {
+        const rows = input.trim().split("\n");
+        const headers = rows[0].split("|").map(header => `<th>${header.trim()}</th>`).join("");
+        const body = rows.slice(2).map(row => {
+            const cols = row.split("|").map(col => `<td>${col.trim()}</td>`).join("");
+            return `<tr>${cols}</tr>`;
+        }).join("");
+
+        return `<table class='table'><thead><tr>${headers}</tr></thead><tbody>${body}</tbody></table>`;
+    }
+
     let html = markdown
         .replace(/```([\s\S]*?)```/gim, (_, p1) => `<pre class='pre'><code>${escapee(p1)}</code></pre>`)
         .replace(/`([\s\S]*?)`/gim, (_, p1) => `<pre class='inline'><code>${escapee(p1)}</code></pre>`)
+        .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img class='img' alt='$1' src='$2' />")
         .replace(/\[(.*?)\]\((.*?)\)/gim, "<a class='a' target='_blank' href='$1'>$2</a>")
         .replace(/^# (.*$)/gim, "<h1 class='h1'>$1</h1>")
         .replace(/^## (.*$)/gim, "<h2 class='h2'>$1</h2>")
@@ -27,6 +40,7 @@ const plaintohtml =(markdown)=>{
         .replace(/^>(.*$)/gim, '<blockquote class="blok" >$1</blockquote>')
         .replace(/<\/blockquote>\n<blockquote>/gim, '<br/>')
         .replace(/^---/gm, "<hr />")
+        .replace(/\n\|?.*\|\n(.*\n)+/gim, parseTable)
         .replace(/\n/gim, "<br />");
 
     return html
